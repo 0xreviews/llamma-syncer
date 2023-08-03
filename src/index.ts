7b5340f01441ccf1f19bc24cf3e933d49c9d155b
Paco <liaoishere@gmail.com>
2023-08-04 02:31:56 +0800

    up and running

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,31 @@
+import express, { Request, Response } from 'express'
 export function sleep(ms: number): Promise<unknown> {
     return new Promise(resolve => setTimeout(resolve, ms))
 }
 
 async function main() {
-    console.log('HEllo world')
-     await sleep(10000)
+    if (!process.env.PORT) {
+        process.exit(1)
+    }
+
+    const PORT = parseInt(process.env.PORT as string, 10)
+
+    const app = express()
+    app.use(express.json())
+
+    app.get('/', (req: Request, res: Response) => {
+        res.status(200).send('Hello World!')
+    })
+
+    app.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}`)
+    })
+
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+        console.log('HEllo world')
+        await sleep(10000)
+    }
 }
 
 main()

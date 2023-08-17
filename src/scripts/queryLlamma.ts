3134be6885d6859e3965286be0d95db871dddd65
Paco <liaoishere@gmail.com>
2023-08-17 16:27:10 +0800

    refine query script

--- src/scripts/queryLlamma.ts
+++ src/scripts/queryLlamma.ts
@@ -21,7 +21,6 @@ const tricryptoPoolAbi = [
 
 // sfrxETH LLAMMA
 // https://etherscan.io/tx/0x1ef2416f06f39fab6498b71ea409baf9b569c064aa3847015fc6e05f7cd68dd7
-const endBlock = 17466899
 const llammaAddress = '0x136e783846ef68c8bd00a3369f787df8d683a696'
 const chainlinkAggAddress = '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419'
 const tricryptoPoolAddress = '0xd51a44d3fae010294c616388b506acda1bfaae46'
@@ -35,7 +34,6 @@ const TRI_CRYPTO_TOKENS = {
     1: ['WBTC', 8],
     2: ['WETH', 18],
 }
-
 
 // // wstETH LLAMMA
 // // https://etherscan.io/tx/0xc3054cb818a625aa437c50b0f3998cb6fec4d63a611e18adcd44e46a063f473f
@@ -54,8 +52,6 @@ const TRI_CRYPTO_TOKENS = {
 //     2: ['WETH', 18],
 // }
 
-
-
 const aggregatorAddress = '0xe5afcf332a5457e8fafcd668bce3df953762dfe7'
 
 const llamma = new Contract(llammaAddress, llammaAbi, provider)
@@ -63,29 +59,9 @@ const chainlinkAgg = new Contract(chainlinkAggAddress, chainlinkAggAbi, provider
 const tricryptoPool = new Contract(tricryptoPoolAddress, tricryptoPoolAbi, provider)
 const aggregator = new Contract(aggregatorAddress, aggregatorAbi, provider)
 
-
-
 const abiCoder = ethers.AbiCoder.defaultAbiCoder()
-async function fetchDataByBlock(blockNumber:number) {
-    const block = await provider.getBlock(blockNumber)
-    let logTxt: string[] = []
-    logTxt.push(`=============== block: ${blockNumber}, timestamp: ${block.timestamp} ===============`)
-
-    const crvusdPrice = formatEther(await aggregator.price({ blockTag: blockNumber }))
-    const tricryptoPoolPrice = formatEther(await tricryptoPool.price_oracle(1, { blockTag: blockNumber }))
-    const tricryptoLastPrice = formatEther(await tricryptoPool.last_prices(1, { blockTag: blockNumber }))
-    const tricryptoLastPriceTimestamp = (
-        await tricryptoPool.last_prices_timestamp({ blockTag: blockNumber })
-    ).toString()
-    const chainlinkPrice = formatUnits((await chainlinkAgg.latestRoundData({ blockTag: blockNumber }))[1], 8)
-    const llammaPrice = formatEther(await llamma.price_oracle({ blockTag: blockNumber }))
-
-    logTxt.push(`crvUSD price: ${crvusdPrice}`)
-    logTxt.push(`chainlink ETH price: ${chainlinkPrice}`)
-    logTxt.push(`tricrypto EMA Price: ${tricryptoPoolPrice}`)
-    logTxt.push(`tricrypto last price: ${tricryptoLastPrice}`)
-    logTxt.push(`tricrypto last price timestamp: ${tricryptoLastPriceTimestamp}`)
-    logTxt.push(`llamma price: ${llammaPrice}`)
+async function fetchDataByBlock(blockNumber: number) {
+    const logTxt: string[] = []
 
     const tricryptoTopics = await tricryptoPool.filters.TokenExchange().getTopicFilter()
     const tricryptoFilter = {
@@ -95,6 +71,47 @@ async function fetchDataByBlock(blockNumber:number) {
         topics: tricryptoTopics,
     }
     const tricryptoLogs = _.sortBy(await provider.getLogs(tricryptoFilter), 'index')
+
+    const llammaTopics = await llamma.filters.TokenExchange().getTopicFilter()
+    const llammaFilter = { fromBlock: blockNumber, toBlock: blockNumber, address: llammaAddress, topics: llammaTopics }
+    const llammaLogs = await provider.getLogs(llammaFilter)
+
+    if (llammaLogs.length === 0 && tricryptoLogs.length === 0) {
+        return []
+    }
+
+    const block = await provider.getBlock(blockNumber)
+    logTxt.push(`=============== block: ${blockNumber}, timestamp: ${block.timestamp} ===============`)
+
+    const overrides = { blockTag: blockNumber }
+    const results = await Promise.all([
+        aggregator.price(overrides),
+        tricryptoPool.price_oracle(1, overrides),
+        tricryptoPool.last_prices(1, overrides),
+        tricryptoPool.last_prices_timestamp(overrides),
+        chainlinkAgg.latestRoundData(overrides),
+        llamma.price_oracle(overrides),
+        llamma.active_band(overrides),
+        llamma.get_p(overrides),
+    ])
+
+    const crvusdPrice = formatEther(results[0])
+    const tricryptoPoolPrice = formatEther(results[1])
+    const tricryptoLastPrice = formatEther(results[2])
+    const tricryptoLastPriceTimestamp = results[3].toString()
+    const chainlinkPrice = formatUnits(results[4][1], 8)
+    const llammaPrice = formatEther(results[5])
+    const activeBand = results[6].toString()
+    const p = formatEther(results[7])
+
+    logTxt.push(`crvUSD price: ${crvusdPrice}`)
+    logTxt.push(`chainlink ETH price: ${chainlinkPrice}`)
+    logTxt.push(`tricrypto EMA Price: ${tricryptoPoolPrice}`)
+    logTxt.push(`tricrypto last price: ${tricryptoLastPrice}`)
+    logTxt.push(`tricrypto last price timestamp: ${tricryptoLastPriceTimestamp}`)
+    logTxt.push(`LLAMMA price: ${llammaPrice} active band: ${activeBand.toString()}, p: ${p.toString()}}`)
+
+    if (tricryptoLogs.length > 0) logTxt.push('3pool txs:')
     for (const log of tricryptoLogs) {
         const trader = ethers.getAddress('0x' + log.topics[1].slice(26))
         const [soldId, tokensSold, boughtId, tokensBought] = abiCoder.decode(
@@ -112,15 +129,8 @@ async function fetchDataByBlock(blockNumber:number) {
         )
     }
 
-    const activeBand = await llamma.active_band({ blockTag: blockNumber })
-    const p = formatEther(await llamma.get_p({ blockTag: blockNumber }))
-    logTxt.push(`LLAMMA: active band: ${activeBand.toString()}, p: ${p.toString()}}`)
-
-    const topics = await llamma.filters.TokenExchange().getTopicFilter()
-    const filter = { fromBlock: blockNumber, toBlock: blockNumber, address: llammaAddress, topics }
-    const logs = await provider.getLogs(filter)
-
-    for (const log of logs) {
+    if (llammaLogs.length > 0) logTxt.push('LLAMMA txs:')
+    for (const log of llammaLogs) {
         const trader = ethers.getAddress('0x' + log.topics[1].slice(26))
         const [soldId, tokensSold, boughtId, tokensBought] = abiCoder.decode(
             ['uint256', 'uint256', 'uint256', 'uint256'],
@@ -132,26 +142,26 @@ async function fetchDataByBlock(blockNumber:number) {
             } for ${formatEther(tokensBought)} ${TOKENS[boughtId]}`,
         )
     }
-    logTxt.push("\n")
+    logTxt.push('\n')
     return logTxt
 }
 
 async function main() {
-
-    const batch_size = 10
-    const total_size = 100;
-    let blockNumber = endBlock - total_size;
+    const endBlock = 17466899
+    const batch_size = 3
+    const total_size = 100
+    let blockNumber = endBlock - total_size
     for (let n = 0; n * batch_size <= total_size; n++) {
-        let promises = []
+        const promises = []
         for (let i = 0; i < batch_size; i++) {
-            if (blockNumber+i > endBlock) break; 
-            promises.push(fetchDataByBlock(blockNumber+i))
+            if (blockNumber + i > endBlock) break
+            promises.push(fetchDataByBlock(blockNumber + i))
         }
-        const results = await Promise.all(promises);
+        const results = await Promise.all(promises)
         results.forEach(logs => {
-            console.log(logs.join("\n"))
+            if (logs.length > 0) console.log(logs.join('\n'))
         })
-        blockNumber += batch_size;
+        blockNumber += batch_size
     }
 }
 
